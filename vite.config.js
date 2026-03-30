299df7bdf5deb7743afa19322db901ecea06331d
Joan Gjergo <joan.gjergo@jet-bank.com>
2026-03-30 10:38:57 +0200

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // Replace 'Token_Generator' with your repo name if different
 export default defineConfig({
-  base: '/Token_Generator/',
+  base: '/Token_Generator-/',
   plugins: [react()],
 })
